797f1a3d56d1305d5acd96c7c804008d699c0580
Sathish S <sathish.amsaravanan@gmail.com>
2024-02-02 03:29:15 +0000

    Added the bubble sort implementation

--- src/sorting/Bubble.java
+++ src/sorting/Bubble.java
@@ -1,8 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main{
+public class Bubble{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = in.nextInt();
+        }
+        int i = 0 , j = 0 , len = arr.length;
+        System.out.println("Unsorted Array : "+ Arrays.toString(arr));
+        while(i < len - 1){
+            j = 0 ;
+            boolean flag = false;
+            while(j < len - i - 1){
+                if(arr[j] > arr[j + 1]){
+                    flag = true;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+                j++;
+            }
+            if(!flag) break;
+            i++;
+        }
+        System.out.println("Sorted Array : "+Arrays.toString(arr));
     }
 }
